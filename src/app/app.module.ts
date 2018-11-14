cbcbbff353fa361bc5845be041ce71a4552ad307
James Lamorte <jameslamorte@Jamess-iMac.local>
2018-11-14 12:41:37 -0600

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list.component';
+
+// tslint:disable-next-line:import-spacing
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
